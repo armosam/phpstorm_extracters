40654dc27d74e8d43d5c0418fff0dbe77520e3f7
Armen Bablanyan <armosam@gmail.com>
2026-08-23 15:32:11 -0500

    Added column name definition in the XML format. Improved value formatting for XML and array extractors.

--- DbToPHPArray-JavaScript.array.js
+++ DbToPHPArray-JavaScript.array.js
@@ -1,51 +1,58 @@
 function eachWithIdx(iterable, f) {
-    var i = iterable.iterator();
-    var idx = 0;
-    while (i.hasNext())
-        f(i.next(), idx++);
+  var i = iterable.iterator();
+  var idx = 0;
+  while (i.hasNext())
+    f(i.next(), idx++);
 }
 function mapEach(iterable, f) {
-    var vs = [];
-    eachWithIdx(iterable, function (i) {
-        vs.push(f(i));
-    });
-    return vs;
+  var vs = [];
+  eachWithIdx(iterable, function (i) {
+    vs.push(f(i));
+  });
+  return vs;
 }
 function escape(str) {
-    str = com.intellij.openapi.util.text.StringUtil.escapeXml(str);
-    return str;
+  str = com.intellij.openapi.util.text.StringUtil.escapeXml(str);
+  return str;
 }
 function quote(str) {
-    return "'" + str + "'";
+  return "'" + str + "'";
 }
 
 var NEWLINE = "\n";
 var table_name = (TABLE) ? `$${TABLE?.getName()}` : '$exported_data';
 
 function output() {
-    for (var i = 0; i < arguments.length; i++) {
-        OUT.append(arguments[i]);
-    }
+  for (var i = 0; i < arguments.length; i++) {
+    OUT.append(arguments[i]);
+  }
 }
 function outputRow(items) {
-    output("\t[", NEWLINE);
-    for (var i = 0; i < items.length; i++) {
-        if ( i > 0 ) { output(',', NEWLINE); }
-        output('\t\t', quote(items[i][0]), ' => ', quote(items[i][1]));
-    }
+  output("\t[", NEWLINE);
 
-    output(NEWLINE, "\t]");
+  for (var i = 0; i < items.length; i++) {
+    if ( i > 0 ) { output(',', NEWLINE); }
+    const col = items[i][0];
+    const val = items[i][1];
+    if (val === 'null' || (!isNaN(val) && !isNaN(parseFloat(val)))) {
+      output('\t\t', quote(col), ' => ', val);
+    } else {
+      output('\t\t', quote(col), ' => ', quote(val));
+    }
+  }
+
+  output(NEWLINE, "\t]");
 }
 
 output(`${table_name} = [`, NEWLINE);
 
 eachWithIdx(ROWS, function (row, idx) {
-    if ( idx > 0 ) {
-        output(',', NEWLINE);
-    }
-    outputRow(mapEach(COLUMNS, function (col) {
-        return [col.name(), FORMATTER.format(row, col)];
-    }));
+  if ( idx > 0 ) {
+    output(',', NEWLINE);
+  }
+  outputRow(mapEach(COLUMNS, function (col) {
+    return [col.name(), FORMATTER.format(row, col)];
+  }));
 });
 
 output(NEWLINE, "];", NEWLINE);
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 PHP Storm extractors from database to different formats
 
-Currently there are two extractors to extract PHPStorm query results to the PHP array and XML format.
+Currently, there are two extractors to extract PHPStorm query results to the PHP array and XML format.
 
 Usage: These extractors could be used in PHPUnit test cases to create XML or PHP datasets directly from database.
 
@@ -13,8 +13,35 @@ To use for example PHP array extractor:
 - run your database query, 
 - select data you need to extract from results and copy to the clipboard.
 
-When you past coppied data it should be in the PHP array format. 
+When you past the copied data it should be in the PHP array format. 
 
 There are same steps for XML extractor just select XML extractor from the list ...
+
+PHP Array format:
+
+```php
+    $<table_name> = [
+        '<column_name>' => '<column_value>',
+        '<column_name>' => null,
+        ...
+    ];
+```
+
+XML format:
+
+```xml
+<?xml version="1.0" ?>
+<dataset>
+    <table name="<table_name>">
+        <column>column_name</column>
+        ...
+        <row>
+            <value name="<column_name>">column_value</value>
+            <null name="<column_name>"/>
+            ...
+        </row>
+    </table>
+</dataset>
+```
 
 Enjoy it for free :)
